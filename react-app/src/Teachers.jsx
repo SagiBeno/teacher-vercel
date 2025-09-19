a3cba72d961d21c800684cc121266238ca8836bb
SagiBeno <benosagi321@gmail.com>
2025-09-19 11:07:55 +0200

    Bypass CORS error

--- react-app/src/Teachers.jsx
+++ react-app/src/Teachers.jsx
@@ -5,12 +5,20 @@ export default class Teachers extends React.Component{
     }
 
     render() {
-        return <div style={{margin: '20px'}}>
-            {JSON.stringify(this.state.teachers)}
-        </div>
+        return (
+            <div style={{margin: '20px'}}>
+                {JSON.stringify(this.state.teachers)}
+            </div>
+        )
     }
 
     componentDidMount() {
-        // TODO - callout for teachers data
+        // callout for teachers data
+        fetch('http://localhost:3333/teachers')
+        .then(console.log)
+        .catch(console.warn)
+        .finally(() => {
+            console.log('fetch done')
+        })
     }
 }
